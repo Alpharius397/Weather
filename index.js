6eca6667246d8f40ca986539ad699256a962794c
Alpharius397 <155239372+Alpharius397@users.noreply.github.com>
2024-01-01 13:23:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,31 @@ const error404 = document.querySelector('.not-found');
 const re = document.getElementById('re');
 const refresh = document.getElementById('refresh');
 var other;
-var wispbox = document.getElementById('wind_speed').checked;
-var visibox = document.getElementById('visi').checked;
-var widibox= document.getElementById('wind_direction').checked;
-var preboxx = document.getElementById('pre').checked;
+
 
 function C_F(celsius) {
     F = 1;
     F = (celsius * 9/5) + 32;
     return F;
   }
+
+function detCnt(check){
+    if (check){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+function pass(div_class,count){
+    if(count<=2){
+        weatherDetails1.appendChild(div_class);
+    }
+    else{
+        weatherDetails2.appendChild(div_class);
+    }
+}
 
 refresh.addEventListener('click', () => {
     if(!(re)){
@@ -44,13 +59,14 @@ refresh.addEventListener('click', () => {
     other.style.display = '';
     other = document.querySelector('.other-info1');
     other.style.display = '';
+    weatherDetails1.innerHTML = ``;
+    weatherDetails2.innerHTML = ``;
 
             container.style.height = '220px';
             weatherBox.style.display = 'none';
             weatherDetails1.style.display = 'none';
             weatherDetails2.style.display = 'none';
             container.style.display = 'block';
-
             container.classList.add('fadeIn');
          
 
@@ -67,8 +83,58 @@ search.addEventListener('click', () => {
     const preboxx = document.getElementById('pre').checked;
     var del = document.querySelector('.other-info');
     var dele = document.getElementById('del');
+    var count = 0;
+    var div_create
+    var Cnt = detCnt(visibox) + detCnt(widibox) + detCnt(wispbox) + detCnt(preboxx);
+    var list = [wispbox,visibox,widibox,preboxx];
 
-   
+    for(i=0;i<4;i++){
+        if(list[i]==true){
+            switch(i){
+                case 0: div_create = document.createElement("div");
+                        div_create.class = "wind";
+                        div_create.id = "wind";
+                        div_create.innerHTML = `<i class="fa-solid fa-wind"></i>
+                        <div class="text">
+                            <span id = "1"></span>
+                            <p>Wind Speed</p>
+                        </div>`; count+=1;
+                        pass(div_create,count); break;
+                case 1: div_create = document.createElement("div");
+                        div_create.class = "vis";
+                        div_create.id = "vis";
+                        div_create.innerHTML = `<i class="fa-solid fa-eye"></i>
+                        <div class="text">
+                            <span id = "2"></span>
+                            <p>Visibilty</p>
+                        </div>`; count+=1;
+                        pass(div_create,count); break;
+
+                case 2: div_create = document.createElement("div");
+                        div_create.class = "wind_dir";
+                        div_create.id = "wind_dir";
+                        div_create.innerHTML = `<i class="fa-brands fa-nfc-directional"></i>
+                        <div class="text">
+                            <span id = "3"></span>
+                            <p>Wind Direction</p>
+                        </div>`; count+=1;
+                        pass(div_create,count); break;
+
+                case 3: div_create = document.createElement("div");
+                        div_create.class = "Pres";
+                        div_create.id = "Pres";
+                        div_create.innerHTML = `<i class="fa-solid fa-gauge"></i>
+                        <div class="text">
+                          <span id = "4"></span>
+                          <p>Pressure</p>
+                       </div>`; count+=1;
+                        pass(div_create,count); break;
+
+                default: console.log('ll');
+            }
+        }
+    }
+
     if (city === '')
         return;
 
@@ -92,56 +158,101 @@ search.addEventListener('click', () => {
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
-            const wind_direction = document.querySelector('.weather-details2 .wind_dir span');
-            const wind = document.querySelector('.weather-details1 .wind span');
-            const visi = document.querySelector('.weather-details1 .vis span');
-            const pres = document.querySelector('.weather-details2 .Pres span');
+            const wind_direction = document.getElementById("3");
+            const wind = document.getElementById("1");
+            const visi = document.getElementById("2");
+            const pres = document.getElementById("4");
   
 
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span> <h9 style="color:white">_</h9>/ ${C_F(parseInt(json.main.temp))}<span>°F</span>`;
             description.innerHTML = `${json.weather[0].description}`;
 
+        try{
             if(widibox){
                 wind_direction.innerHTML = `${json.wind.deg}°`;
             }
             else{
                 wind_direction.innerHTML = `-`;
             }
+        }
 
+        catch(TypeError){
+
+        }
+
+        try{
             if(wispbox){
                 wind.innerHTML = `${parseInt(json.wind.speed)} Km/h`;
             }
             else{
                 wind.innerHTML = `-`;
             }
+        }
 
+        catch(TypeError){
+
+        }
+
+        try{
             if(visibox){
                 visi.innerHTML = `${parseInt(json.visibility)} Km`;
             }
             else{
                 visi.innerHTML = `-`;
             }
+        }
+        catch(TypeError){
 
+        }
+
+        try{
             if(preboxx){
                 pres.innerHTML = `${parseInt(json.main.pressure)} hPa`;
             }
             else{
                 pres.innerHTML = `-`;
             }
+        }
+
+        catch(TypeError){
+
+        }
+
 
             weatherBox.style.display = '';
+            other = document.getElementById('del');
+            other.style.display = 'none';
             other = document.getElementById('del');
             other.style.display = 'none';
             other = document.querySelector('.other-info');
             other.style.display = 'none';
             other = document.querySelector('.other-info1');
             other.style.display = 'none';
-            weatherDetails1.style.display = '';
-            weatherDetails2.style.display = '';
             weatherBox.classList.add('fadeIn');
-            weatherDetails1.classList.add('fadeIn');
-            weatherDetails2.classList.add('fadeIn');
-            container.style.height = '460px';
+
+            
+            if(Cnt==0){
+                weatherDetails1.style.display = 'none';
+                weatherDetails2.style.display = 'none';
+                weatherDetails1.classList.add('fadeIn');
+                weatherDetails2.classList.add('fadeIn');
+                container.style.height = '280px';
+            }
+            else if(Cnt==1 || Cnt==2){
+                weatherDetails1.style.display = '';
+                weatherDetails2.style.display = 'none';
+                weatherDetails1.classList.add('fadeIn');
+                weatherDetails2.classList.add('fadeIn');
+                container.style.height = '360px';
+            }
+            else{
+                weatherDetails1.style.display = '';
+                weatherDetails2.style.display = '';
+                weatherDetails1.classList.add('fadeIn');
+                weatherDetails2.classList.add('fadeIn');
+
+                container.style.height = '440px';
+            }
 
 
         });
